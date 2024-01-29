74f9fbec40583e17e469f18e984ab88fe4367946
pupil <pupil@ivkhk.ee>
2024-01-29 15:16:22 +0200

    небольшие изменения

--- src/javafxstore/JavaFXStore.java
+++ src/javafxstore/JavaFXStore.java
@@ -8,6 +8,7 @@ package javafxstore;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -18,27 +19,14 @@ import javafx.stage.Stage;
  * @author pupil
  */
 public class JavaFXStore extends Application {
-    
+     private Stage primaryStage;
+     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage primaryStage) throws Exception {
+        setPrimaryStage(primaryStage);
+        this.primaryStage.setTitle("JKTVFXLibrary");
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("home.fxml"));
     }
 
     /**
@@ -46,6 +34,9 @@ public class JavaFXStore extends Application {
      */
     public static void main(String[] args) {
         launch(args);
+    }
+    public void setPrimaryStage(Stage primaryStage) {
+        this.primaryStage = primaryStage;
     }
     
 }
